60642ba6ef92aa3b679c73e90d360cfa77903d94
bithe <farzana.hossain5@gmail.com>
2022-02-16 19:53:18 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
+function updatePrice(itemId, price) {
+    document.getElementById(itemId).addEventListener("click", function() {
+
+        const memoryCost = document.getElementById('memory-cost');
+        memoryCost.innerText = price;
+
+        const bestPrice = document.getElementById('best-price').innerText;
+        const memoryPrice = document.getElementById('memory-cost').innerText;
+        const storagePrice = document.getElementById('storage-cost').innerText;
+        const delieryPrice = document.getElementById('delivery-cost').innerText;
+
+        const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(delieryPrice);
+
+        const totalPriceText = document.getElementById('total-price');
+        totalPriceText.innerText = totalPrice.innerText;
+        console.log(totalPrice)
+    });
+}
+
+///--------------- ---------------///
 document.getElementById('sixteen-memory').addEventListener("click", function() {
 
     const memoryCost = document.getElementById('memory-cost');
     memoryCost.innerText = 200;
 
     const bestPrice = document.getElementById('best-price').innerText;
-    const memoryCost = document.getElementById('memory-cost').innerText;
-    const storageCost = document.getElementById('storage-cost').innerText;
-    const delieryCost = document.getElementById('delivery-cost').innerText;
+    const memoryPrice = document.getElementById('memory-cost').innerText;
+    const storagePrice = document.getElementById('storage-cost').innerText;
+    const delieryPrice = document.getElementById('delivery-cost').innerText;
 
-    const totalPrice = parseFloat(bestPrice) + parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(delieryCost);
+    const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(delieryPrice);
+
+    const totalPriceText = document.getElementById('total-price');
+    totalPriceText.innerText = totalPrice.innerText;
+    console.log(totalPrice)
+});
+
+///------------  ------------///
+document.getElementById('two-fifty-six-storage').addEventListener("click", function() {
+
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerHTML = 200;
 });
