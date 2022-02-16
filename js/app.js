8c18bee479649948300174153fd6f32143046d33
bithe <farzana.hossain5@gmail.com>
2022-02-16 11:33:30 +0600

    btn take input

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 document.getElementById('login-btn').addEventListener("click", function() {
-    const document.getElementById('')
-    console.log("clicked");
+    const emailInput = document.getElementById('email-input').value;
+    const passwrodInput = document.getElementById('password-input').value;
+    console.log("clicked", emailInput, passwrodInput);
+
+    if (emailInput === 'admin.gmail.com' && passwrodInput === 1234) {
+        console.log('done');
+    }
 });
